97ec351e9ee10559745d4d587027f5b79ef93fc6
Jordan Odoemenam <jordanodo89@gmail.com>
2023-07-04 06:43:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
-const cors = require('cors');
-const corsOptions ={
-    origin:'https://client-wdmg.onrender.com', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-
 app.get("/", (req,res)=>{
    
 })

1a4f65f3cdbe6ff2febbbe39ce98426300a3f410
Jordan Odoemenam <jordanodo89@gmail.com>
2023-07-04 08:49:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(cors())
 
-app.options('*', cors());
-
-app.get("/", cors(), (req,res)=>{
-     origin: "https://client-wdmg.onrender.com",
-     res.setHeader("Access-Control-Allow-Origin", "https://client-wdmg.onrender.com"); // Replace * with the appropriate origin
-     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    optionsSuccessStatus: 200,
-})
-
-  
+app.get("/", cors(), (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://client-wdmg.onrender.com"); // Replace * with the appropriate origin
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.status(200).send("OK");
+});
 
 app.post('/login', async(req,res)=>{
     const{username,password} = req.body
